f579797d3463cd227109f35c6729ce9d86b86610
Gilbert <gilbert@Gilberts-MacBook-Pro.local>
2019-11-26 20:55:32 -0500

    spark convertion files

--- constract_graph.py
+++ constract_graph.py
@@ -6,8 +6,8 @@ spark = SparkSession \
     .appName("Graph") \
     .getOrCreate()
 
-df = spark.read.option("header", "true").csv("airlines.csv")
-df.write.parquet("airline_parquet")
+# df = spark.read.option("header", "true").csv("2016_full.csv")
+# df.write.parquet("2016_full_parquet")
 
 
 
@@ -15,13 +15,13 @@ df.write.parquet("airline_parquet")
 
 # ------------------------------TEST----------------------------------------------
 # The result of loading a parquet file is also a DataFrame.
-# parquetFile = spark.read.parquet("airline_parquet")
+parquetFile = spark.read.parquet("2016_full_parquet")
 #
 # # Parquet files can also be used to create a temporary view and then used in SQL statements.
-# parquetFile.createOrReplaceTempView("parquetFile")
-# everything = spark.sql("SELECT * from parquetFile limit 10")
+parquetFile.createOrReplaceTempView("parquetFile")
+everything = spark.sql("SELECT * from parquetFile limit 10")
 #
-# everything.show()
+everything.show()
 
 
 
