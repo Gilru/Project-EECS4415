df8578f22198f9e275af4f2d669478d040432104
Gilbert <gilbert@Gilberts-MacBook-Pro.local>
2019-11-26 19:26:13 -0500

    second commit

--- constract_graph.py
+++ constract_graph.py
@@ -4,8 +4,7 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession \
     .builder \
-    .appName("Python Spark SQL basic example") \
-    .config("spark.some.config.option", "some-value") \
+    .appName("Graph") \
     .getOrCreate()
 
 
